0f23073a30b70e1e9d3b9d7329b0df5414d7a063
snufka <52801250+snufka@users.noreply.github.com>
2019-09-30 21:49:47 +0200

    commit

---
+++ PREWORK_AFH/MODULE-6/Javascript.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+    
+    });
+    
+    document.getElementById("button4").addEventListener("click", function(){
+    
+    document.getElementById("box").style.height = "150px";
+    
+    });
